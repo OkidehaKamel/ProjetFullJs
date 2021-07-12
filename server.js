c47b01856b16a215890983d25497d8014c6de290
Sanita <rattanasamaysanita@yahoo.fr>
2021-07-12 22:37:09 +0200

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+// const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT || 5001
+
 const app = express()
 const path=require('path')
+
+// mongoose.connect('mongodb://localhost/projet',{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
 
 app.use(express.json())
 
@@ -14,6 +21,7 @@ app.get('/api/test',(req,res)=>{
     })
 })
 
+//default page
 app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,'./front/build/index.html'))
 })
